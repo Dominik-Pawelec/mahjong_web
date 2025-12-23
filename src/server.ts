75df0df68f2a67ec655b2aa2d09c98922d9e1d91
Dominik-Pawelec <dominikpawelec@gmail.com>
2025-12-23 22:29:23 +0100

    basic server socket.io construction, added views folder

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+import type { Request, Response } from "express";
+
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
+const io = require("socket.io")(server);
+
+const port = 6060;
+app.set("view engine", "ejs");
+
+
+app.get("/game", (req : Request, res : Response) => {
+    res.render("game");
+});
+
+server.listen(port, () => {
+    console.log("server runs at " + port);
+});
+
+
+//socket.io logic
+io.on("connection", (socket : any) => { //TODO: make proper typing, not any
+    console.log("User connected :" + socket.id);
+});
+
