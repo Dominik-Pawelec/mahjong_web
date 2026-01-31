f0b19a1799221ba70599298ee0f9b76b8c03f119
frogrammer9 <oskar@meler.pl>
2026-01-31 19:07:43 +0100

    fix gitignore

---
+++ src/frontend/svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
+		typescript: {
+		  config: (cfg) => {
+			cfg.include.push("../common/**/*.ts");
+			return cfg;
+		  }
+		},
+		alias: {
+			"@common": "../common",
+			$assets: "static/assets"
+		}
+	}
+};
+
+export default config;
+
